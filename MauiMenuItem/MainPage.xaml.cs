4315bae06b5911b0d7ac7b163c651f15541c4cd9
usa-33-pyon <chiho@mac-mini.lan>
2023-08-13 16:38:45 +0900

    [MAUI] [Android] モーダル上に表示されているリスト項目を長押しした際の挙動の確認用

---
+++ MauiMenuItem/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiMenuItem;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnButtonClicked(object sender, EventArgs e)
+	{
+		var nextPage = new NavigationPage(new NewPage1());
+		Navigation.PushModalAsync(nextPage);
+	}
+}
+
+
---
+++ MauiMenuItem/NewPage1.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace MauiMenuItem;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+
+        var list = new List<Item>();
+        list = Enumerable.Range(1, 5)
+                .Select(i => new Item { DisplayName = "item" + i })
+                .ToList();
+        listView.ItemsSource = list;
+    }
+
+    void OnMenuItemClicked(System.Object sender, System.EventArgs e)
+    {
+		DisplayAlert(null, "Alert Message", "OK");
+    }
+
+    void OnCloseButtonClicked(System.Object sender, System.EventArgs e)
+    {
+        Navigation.PopModalAsync();
+    }
+}
+
+public class Item
+{
+    public string DisplayName { get; set; }
+}
